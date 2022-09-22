9de12cb4b1e867757ee42ac0fffbfb6bb27e8c52
ZynpHlt <69972906+ZeynepHlt@users.noreply.github.com>
2022-09-22 22:16:20 +0300

    vue-cli oluşturuldu

--- LESSONS/Hafta-2/1-vue-cli/src/App.vue
+++ LESSONS/Hafta-2/1-vue-cli/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header />
+  <div class="container">
+    <h3>Bu benim ilk Vue CLI Uygulamam</h3>
+    <p>Bugün component ve Vue CLI konularını görüyoruz..</p>
+  </div>
+  <!-- <CounterItem></CounterItem> -->
+  <counter-item />
+  <counterItem />
+  <!-- <benim-guzel-sayacim></benim-guzel-sayacim> -->
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CounterItem from "@/components/CounterItem";
+import appHeader from "./components/appHeader.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    appHeader,
+    CounterItem,
+    // "benim-guzel-sayacim": CounterItem,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ LESSONS/Hafta-2/1-vue-cli/src/components/appHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>kablosuzkedi bootcamp Vue!</header>
+</template>
+
+<style>
+header {
+  background-color: rebeccapurple;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  border-bottom: 2px solid rgb(5, 5, 5);
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 100px;
+}
+</style>
